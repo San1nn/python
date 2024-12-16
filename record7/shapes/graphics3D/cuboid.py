1f1160e22e135a3dcddbd8ceab70390e27edc8a3
San1nn <107739827+San1nn@users.noreply.github.com>
2024-12-16 12:26:49 +0530

    Add files via upload

---
+++ record7/shapes/graphics3D/cuboid.py
@@ -0,0 +1,6 @@
+def area(length,width,height):
+    return 2*(length * width + width * height + height * length)
+
+def perimeter(length,width,height):
+    return 4 * (length + width + height)
+    
---
+++ record7/shapes/graphics3D/sphere.py
@@ -0,0 +1,8 @@
+import math
+
+def area(radius):
+    return 4 * math.pi * radius ** 2
+
+def perimeter(radius):
+    return 2 * math.pi * radius
+
